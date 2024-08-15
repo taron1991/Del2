c0af280c9dd99e4a7f0b7fa2bebef45495943bc3
Sagirov Airat <airat210496@mail.ru>
2024-08-15 22:26:55 +0400

    renamed method forking to loopArray, changed void to array String, in class TestGitFork, branch dev-Airat

--- src/main/java/com/example/demo/TestGitFork.java
+++ src/main/java/com/example/demo/TestGitFork.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 public class TestGitFork {
-    public void forking(String message) {
+    public String[] loopArray(String message) {
         String[] strings = {"Airat", "Aigel", "Taron", "Vasya"};
         for (int i = 0; i < 10; i++) {
             System.out.println(message);
         }
+        return strings;
     }
 }
