ac54c10bbc731000a85059967b811c25781bc85d
Sagirov Airat <airat210496@mail.ru>
2024-08-15 22:20:41 +0400

    add loop in  method forking, in class TestGitFork, branch dev-Airat

--- src/main/java/com/example/demo/TestGitFork.java
+++ src/main/java/com/example/demo/TestGitFork.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 public class TestGitFork {
     public void forking(String message) {
-        System.out.println(message);
+        String[] strings = {"Airat", "Aigel", "Taron", "Vasya"};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(message);
+        }
     }
 }
